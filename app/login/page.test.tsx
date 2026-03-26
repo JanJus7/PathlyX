373f81780a942476734a3c6ef1851a95f4d49da6
Jan <janjus2004@gmail.com>
2026-03-26 23:41:08 +0100

    fixing login test and adding coverage to package.json

--- app/login/page.test.tsx
+++ app/login/page.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import LoginPage from "./page";
 
-describe("Strona Logowania (/login)", () => {
-  it("powinna wyświetlać przycisk logowania przez Google", () => {
+describe("Login page (/login).", () => {
+  it("It should display the Google login button", () => {
     render(<LoginPage />);
 
     const loginButton = screen.getByRole("button", {
-      name: /zaloguj przez google/i,
+      name: /Sign in with Google/i,
     });
 
     expect(loginButton).toBeInTheDocument();
