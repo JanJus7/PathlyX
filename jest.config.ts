8da6e84960175a9f955ef2466b2b204b8ca846b3
Jan <janjus2004@gmail.com>
2026-03-30 16:48:55 +0200

    configuring jest coverage

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,15 @@ const config: Config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
+
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      functions: 95,
+      lines:95,
+    }
+  }
 }
 
 export default createJestConfig(config)
