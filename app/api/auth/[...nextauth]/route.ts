67f3b4d7ad0abf3444913a9d6c43db07b1c84915
Jan <janjus2004@gmail.com>
2026-04-09 12:05:39 +0200

    small fix - added next auth secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -19,6 +19,7 @@ declare module "next-auth/jwt" {
 }
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
